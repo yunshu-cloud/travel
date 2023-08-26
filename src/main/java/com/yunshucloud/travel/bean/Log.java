290583c507482d188c39b48ea8ce1c07d9be8801
yunshu-cloud <w2099266050@gmail.com>
2023-08-26 23:19:21 +0800

    aop日志记录

---
+++ src/main/java/com/yunshucloud/travel/bean/Log.java
@@ -0,0 +1,16 @@
+package com.yunshucloud.travel.bean;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Log {
+    private String url; // 访问的路径
+    private Date visitTime; // 访问时间
+    private String username; // 访问者
+    private String ip; // 访问ip
+    private int executionTime; // 访问时长
+    private String exceptionMessage; // 异常信息
+}
+
