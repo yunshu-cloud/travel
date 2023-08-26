f948ed225de0a5692779ce790d45f1e5a34ddf6b
yunshu-cloud <w2099266050@gmail.com>
2023-08-26 22:53:06 +0800

    事务

--- src/main/java/com/yunshucloud/travel/service/CategoryService.java
+++ src/main/java/com/yunshucloud/travel/service/CategoryService.java
@@ -5,10 +5,12 @@ import com.yunshucloud.travel.mapper.CategoryMapper;
 import com.yunshucloud.travel.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
--- src/main/java/com/yunshucloud/travel/service/ProductService.java
+++ src/main/java/com/yunshucloud/travel/service/ProductService.java
@@ -5,10 +5,12 @@ import com.yunshucloud.travel.mapper.ProductMapper;
 import com.yunshucloud.travel.pojo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ProductService {
     @Autowired
     private ProductMapper productMapper;
