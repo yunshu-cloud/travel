6f81e6e8ff2715560c9d688aef64bda20ad9ccec
yunshu-cloud <w2099266050@gmail.com>
2023-08-27 12:51:17 +0800

    发送邮件配置

---
+++ src/main/java/com/yunshucloud/travel/mapper/MemberMapper.java
@@ -0,0 +1,9 @@
+package com.yunshucloud.travel.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yunshucloud.travel.pojo.Member;
+
+public interface MemberMapper extends BaseMapper<Member>
+{
+
+}
--- src/test/java/com/yunshucloud/travel/TravelApplicationTests.java
+++ src/test/java/com/yunshucloud/travel/TravelApplicationTests.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yunshucloud.travel.mapper.AdminMapper;
 import com.yunshucloud.travel.pojo.Admin;
 import com.yunshucloud.travel.service.AdminService;
+import com.yunshucloud.travel.util.MailUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,9 @@ class TravelApplicationTests
     @Autowired
     private AdminMapper adminMapper;
 
+    @Autowired
+    private MailUtils mailUtils;
+
     @Test
     void contextLoads()
     {
@@ -31,5 +35,16 @@ class TravelApplicationTests
         System.out.println(desc);
 
     }
+
+
+    /**
+     * 测试邮件发送
+     */
+    @Test
+    void sendMail() {
+        mailUtils.sendMail("2099266050@qq.com","这是一封测试邮件","测试");
+        System.out.println("Send Successfully!");
+    }
+
 
 }
