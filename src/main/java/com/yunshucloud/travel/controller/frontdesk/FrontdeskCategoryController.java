643a46c9c85c4a8b11087c29dde43238f5622a25
yunshu-cloud <w2099266050@gmail.com>
2023-08-27 13:51:35 +0800

    导航栏根据数据库内容动态显示

---
+++ src/main/java/com/yunshucloud/travel/controller/frontdesk/FrontdeskCategoryController.java
@@ -0,0 +1,23 @@
+package com.yunshucloud.travel.controller.frontdesk;
+
+import com.yunshucloud.travel.pojo.Category;
+import com.yunshucloud.travel.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/frontdesk/category")
+public class FrontdeskCategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    @RequestMapping("/all")
+    @ResponseBody
+    public List<Category> all() {
+        return categoryService.findAll();
+    }
+}
+
