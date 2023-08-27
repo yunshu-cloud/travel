899a3db16b2e51c8b1af0123d9dd29a2df97dfd1
yunshu-cloud <w2099266050@gmail.com>
2023-08-27 13:20:12 +0800

    前台用户登录

--- src/main/java/com/yunshucloud/travel/controller/frontdesk/MemberController.java
+++ src/main/java/com/yunshucloud/travel/controller/frontdesk/MemberController.java
@@ -37,4 +37,32 @@ public class MemberController {
         }
         return modelAndView;
     }
+
+
+
+    @RequestMapping("/active")
+    public ModelAndView active(String activeCode) {
+        ModelAndView modelAndView = new ModelAndView();
+        String active = memberService.active(activeCode);
+        modelAndView.addObject("message",active);
+        modelAndView.setViewName("/frontdesk/active_result");
+        return modelAndView;
+    }
+
+
+    @RequestMapping("/login")
+    public ModelAndView register(String name,String password,HttpSession session){
+        ModelAndView modelAndView = new ModelAndView();
+        Result result = memberService.login(name,password);
+        if(!result.isFlag()){ // 登录失败
+            modelAndView.addObject("message",result.getMessage());
+            modelAndView.setViewName("/frontdesk/login");
+        }else{ // 登录成功
+            session.setAttribute("member",result.getData()); // 将用户信息存入session
+            modelAndView.setViewName("redirect:/frontdesk/index");
+        }
+        return modelAndView;
+    }
+
+
 }
--- src/main/java/com/yunshucloud/travel/service/MemberService.java
+++ src/main/java/com/yunshucloud/travel/service/MemberService.java
@@ -4,11 +4,14 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yunshucloud.travel.bean.Result;
 import com.yunshucloud.travel.mapper.MemberMapper;
 import com.yunshucloud.travel.pojo.Member;
+import com.yunshucloud.travel.util.MailUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MemberService {
@@ -16,6 +19,12 @@ public class MemberService {
     private MemberMapper memberMapper;
     @Autowired
     private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    private MailUtils mailUtils;
+
+    @Value("${project.path}")
+    private String projectPath;
 
 
     // 注册
@@ -54,12 +63,85 @@ public class MemberService {
         member.setPassword(password);
         // 设置用户状态为false
         member.setActive(false);
-        // 保存用户
-        memberMapper.insert(member);
-        return new Result(true,"注册成功！");
 
 
         // 2.发送激活邮件
+        // 生成激活码
+        String activeCode = UUID.randomUUID().toString();
+        // 给用户的邮箱发送一封邮件，该邮件包含一个链接，链接中包含激活码
+        String activeUrl = projectPath+"/frontdesk/member/active?activeCode="+activeCode;
+        String text = "恭喜您注册成功！<a href = '"+activeUrl+"'>点击激活</a>完成账号认证";
+        mailUtils.sendMail(member.getEmail(),text,"旅游网激活邮件");
+
+        // 保存激活码，激活时比对
+        member.setActiveCode(activeCode);
+        // 保存用户
+        memberMapper.insert(member);
+        return new Result(true,"注册成功！");
     }
+
+
+    // 激活用户
+    public String active(String activeCode){
+        // 根据激活码查询用户
+        QueryWrapper<Member> queryWrapper = new QueryWrapper();
+        queryWrapper.eq("activeCode",activeCode);
+        Member member = memberMapper.selectOne(queryWrapper);
+        // 没有找到用户：激活失败
+        if(member == null){
+            return "激活失败！激活码错误!";
+        }else {
+            member.setActive(true);
+            memberMapper.updateById(member);
+            return "激活成功，请<a href='"+projectPath+"/frontdesk/login'>登录</a>";
+        }
+    }
+
+
+    public Result login(String name,String password){
+        Member member = null;
+
+        // 查询用户名
+        if (member == null){
+            QueryWrapper<Member> queryWrapper = new QueryWrapper();
+            queryWrapper.eq("username",name);
+            member = memberMapper.selectOne(queryWrapper);
+        }
+
+        // 查询手机
+        if (member == null){
+            QueryWrapper<Member> queryWrapper = new QueryWrapper();
+            queryWrapper.eq("phoneNum",name);
+            member = memberMapper.selectOne(queryWrapper);
+        }
+
+        // 查询邮箱
+        if (member == null){
+            QueryWrapper<Member> queryWrapper = new QueryWrapper();
+            queryWrapper.eq("email",name);
+            member = memberMapper.selectOne(queryWrapper);
+        }
+
+
+        // 没有查询到用户
+        if(member == null){
+            return new Result(false,"用户名或密码错误");
+        }
+
+        // 验证密码
+        boolean flag = encoder.matches(password, member.getPassword());
+        if(!flag){
+            return new Result(false,"用户名或密码错误");
+        }
+
+        // 验证是否激活
+        if(!member.isActive()){
+            return new Result(false,"用户未激活，请登录邮箱激活用户！");
+        }
+
+        return new Result(true,"登录成功！",member);
+    }
+
+
 }
 
